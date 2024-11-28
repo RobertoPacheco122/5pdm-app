46d442af51ae547fbae1bdd19245fe93c9012172
Roberto Pacheco <Robertoaraujo230@hotmail.com>
2024-11-28 08:37:01 -0300

    Initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-slate-200">
+      <Text>Hello World</Text>
+    </View>
+  );
+}
